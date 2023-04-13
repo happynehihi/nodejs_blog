1954e35bc1ed2694fe9b90a47c8dc986f9b259c3
happynehihi <lephanhoangphuc2002@gmail.com>
2023-04-13 19:19:47 +0700

    fix

--- src/database/init.js
+++ src/database/init.js
@@ -1,30 +1,30 @@
 import { connection } from './connection.js';
 
 const createTable = () => {
-    const sql = `
+  const sql = `
     CREATE TABLE IF NOT EXISTS users (
       id INT AUTO_INCREMENT PRIMARY KEY,
       name VARCHAR(255),
       email VARCHAR(255)
     )
   `;
-    connection.query(sql, (err) => {
-        if (err) throw err;
-        console.log('Table created!');
-    });
+  connection.query(sql, (err) => {
+    if (err) throw err;
+    console.log('Table created!');
+  });
 };
 
 const insertData = () => {
-    const sql = `
+  const sql = `
     INSERT INTO users (name, email)
     VALUES ('Le Thi Phuc', 'thiphuc@gmail.com'),
            ('Le Phan Hoang Phuc', 'hoangphuc@gmail.com'),
            ('Le Anh Quoc', 'quoc@gmail.com')
   `;
-    connection.query(sql, (err) => {
-        if (err) throw err;
-        console.log('Data inserted!');
-    });
+  connection.query(sql, (err) => {
+    if (err) throw err;
+    console.log('Data inserted!');
+  });
 };
 
 createTable();
